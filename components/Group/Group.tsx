a51689d44acc51b8af0577486b3d880fcbea4075
turjojoadder <turjojoadder10@gmail.com>
2024-11-01 03:06:17 +0600

    rtk query with pagination and scrolling  done

--- components/Group/Group.tsx
+++ components/Group/Group.tsx
@@ -40,7 +40,7 @@ export default function Group() {
       console.log('Loading more posts for page:', page + 1);
       setPage(prevPage => prevPage + 1);
     }
-  }, [hasMorePosts, isSuccess, isFetching, isError, page]);
+  }, [hasMorePosts, isFetching, isError, page]);
 
   const renderItem = ({item}) => (
     <View style={styles.postContainer}>
@@ -51,12 +51,11 @@ export default function Group() {
   return (
     <View style={styles.container}>
       {/* Centered ActivityIndicator for initial load */}
-    {isLoading && (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    )}
-      {isError && <Text style={styles.errorText}>Failed to fetch posts.</Text>}
+      {isLoading && (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )}
 
       <FlatList
         data={allPosts}
@@ -69,15 +68,19 @@ export default function Group() {
             <ActivityIndicator size="large" color="#0000ff" />
           )
         }
+        // Conditionally show the ListEmptyComponent only if data has been fetched
+        ListEmptyComponent={() =>
+          !isLoading && !isFetching ? (
+            <Text style={styles.emptyText}>No posts available</Text>
+          ) : null
+        }
+        contentContainerStyle={allPosts.length === 0 ? styles.emptyList : null} // Handle empty list styling
+      
+    
+      
+        />
 
-            // Conditionally show the ListEmptyComponent only if data has been fetched
-            ListEmptyComponent={() =>
-              !isLoading && !isFetching ? (
-                <Text style={styles.emptyText}>No posts available</Text>
-              ) : null
-            }
-        contentContainerStyle={allPosts.length === 0  ? styles.emptyList : null} // Handle empty list styling
-      />
+      {isError && <Text style={styles.errorText}>Failed to fetch posts.</Text>}
     </View>
   );
 }
@@ -87,14 +90,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     padding: 10,
-    
   },
 
-  loadingContainer:{
-    ...StyleSheet.absoluteFillObject,  // This covers the entire screen
+  loadingContainer: {
+    ...StyleSheet.absoluteFillObject, // This covers the entire screen
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',  // Optional: adds a light overlay effect
+    backgroundColor: 'rgba(255, 255, 255, 0.3)', // Optional: adds a light overlay effect
   },
   postContainer: {
     padding: 15,
